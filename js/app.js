fa623a45a023638444b9c0b6cd8176aa7ea8a062
Vapak <46297246+Vapak@users.noreply.github.com>
2020-10-13 00:20:38 +0700

    parse all fields

--- js/app.js
+++ js/app.js
@@ -11,7 +11,6 @@ function getFile(input){
     let data = reader.result;
     json = JSON.parse(data);
     createForm();
-    // console.log(value);
     };
 
     reader.onerror = function(){
@@ -25,13 +24,115 @@ function createForm(){
   let label = document.createElement('h1');
   label.textContent = json.name.toString();
   label.classList.add('display-2');
+  label.classList.add('mb-4');
+  label.classList.add('text-center');
   divForm.parentNode.prepend(label);
 
   form = document.createElement('form');
   json.fields.forEach((item, i) => {
-    form.append(createField(item));
+    form.append(createField(item.input, item?.label));
   });
   divForm.replaceWith(form);
+  addMasks();
+}
+
+function createField(input, label){
+  let elem = document.createElement('div');
+  let labelElem;
+
+  let inputElem;
+  input.type==="textarea" ? inputElem = document.createElement('textarea') :inputElem = document.createElement('input');
+
+  //ParseLabel
+  if(label !== undefined)
+  {
+      labelElem = document.createElement('label');
+      labelElem.textContent = label;
+  }
+  //ParseInput
+  //type
+  //"number" to "text" for correct masks
+  input.type.toString()==="number" ? inputElem.type = "text" : inputElem.type = input.type.toString();
+
+  //parse technologies
+  if(input.type.toString()==="technology"){
+    labelElem.classList.add("h5");
+    elem.append(labelElem);
+    input.type = "checkbox";
+    input.technologies.forEach((item, i) => {
+      elem.append(createField(input, item));
+    });
+    return elem;
+  }
+
+  //parse colors
+  let colorList;
+
+  if(input.type.toString()==="color"){
+    input.type = "color";
+    colorList = document.createElement('datalist');
+    colorList.setAttribute("id", "colors");
+    input.colors.forEach((item, i) => {
+      let optionElem = document.createElement('option');
+      optionElem.setAttribute("value", item);
+      colorList.append(optionElem);
+    });
+    inputElem.setAttribute("list", "colors");
+  }
+
+  //filetype
+  if(input.type.toString()==="file" && input?.filetype!==undefined){
+    input.filetype.forEach((item, i) => {
+      i=0 ? inputElem.accept += `.${item}`: inputElem.accept+=`, .${item}`;
+    });
+  }
+
+  //isRequired
+  if (Boolean(input?.required)!==undefined) inputElem.required = true;
+
+  //isMultiple
+  if (Boolean(input?.multiple)!==undefined) inputElem.multiple = true;
+
+  //mask
+  if (Boolean(input?.mask!==undefined))
+  inputElem.setAttribute("data-mask", input.mask);
+
+  //colors
+  // if(label.toString().contains("#"))
+  //   inputElem.setAttribute("style", `background-color: ${label};`);
+  //placeholder
+  if (Boolean(input?.placeholder!==undefined))
+  inputElem.placeholder = input.placeholder;
+
+
+  //for checkbox input first
+  if (label!==undefined) elem.append(labelElem);
+  input.type==="checkbox" ? elem.prepend(inputElem) : elem.append(inputElem);
+
+  //set colorlist for "color"
+  if(input.type.toString() === "color"){
+    elem.append(colorList);
+  }
+
+  addClasses(elem, inputElem, labelElem);
+
+  return elem;
+}
+
+function addClasses(div, input, label){
+  if(input.type === "checkbox"){
+    div.classList.add("form-check");
+    input.classList.add("form-check-input");
+    label?.classList.add("form-check-label");
+  }
+  else{
+    div.classList.add("form-group");
+    input.classList.add("form-control");
+    label?.classList.add("h5");
+  }
+}
+
+function addMasks(){
   let count = $("input[data-mask]").length;
   for(let i = 0;i < count;i++)
   {
@@ -39,38 +140,6 @@ function createForm(){
   }
 }
 
-function createField(field){
-  //ParseLabel
-  let labelElem;
-  if(field?.label !== undefined)
-  {
-      labelElem = document.createElement('label');
-      labelElem.textContent = field.label;
-  }
-  let inputElem = document.createElement('input');
+function createButton(){
 
-  if(field.input.type.toString()==="number"){
-    inputElem.type = "text";
-
-  }
-  else {
-      inputElem.type = field.input.type.toString();
-  }
-  inputElem.classList.add('form-control');
-
-  //isRequired
-  Boolean(field.input?.required) ? inputElem.required = true : inputElem.disabled = true;
-
-  //mask
-  if (Boolean(field.input?.mask!==undefined))
-  inputElem.setAttribute("data-mask", field.input.mask);
-
-  //ParseInput
-  let elem = document.createElement('div');
-  elem.classList.add("form-group");
-
-  if (field?.label) elem.append(labelElem);
-  elem.append(inputElem);
-
-  return elem;
 }
